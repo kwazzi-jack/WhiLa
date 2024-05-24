fd12b86f54eacde858f9778b876f2d3f43716d51
Brian Welman <brianallisterwelman@gmail.com>
2024-05-24 11:12:11 +0200

    Cleaned up to focus on an overall class

--- whila/appify.py
+++ whila/appify.py
@@ -1,72 +1,48 @@
-import os
 from pathlib import Path
 
+import gradio
 import modal
-from modal import App, Image
+from modal import App, Image, Volume
 
 from whila.textify import Textifier
 
-_DEFAULT_MODEL_DIR = Path("models")
+_DEFAULT_VOLUME = {"/whila": Volume.from_name("whila-volume")}
+_DEFAULT_MODEL_DIR = Path("/whila/models")
+_DEFAULT_TTS_MODEL = "openai/whisper-medium.en"
+_PIP_PACKAGES = [
+    "git+https://github.com/kwazzi-jack/WhiLa.git",
+    "torch",
+    "transformers",
+]
+_DEFAULT_PYVERSION = "3.10"
+_DEFAULT_IMAGE = Image.debian_slim(python_version=_DEFAULT_PYVERSION)
+_DEFAULT_GPU_CONFIG = modal.gpu.L4()
 
 
-def download_model_to_image(
-    model_name: str, model_dir: str | Path = _DEFAULT_MODEL_DIR
-) -> None:
-    """Based on [1] from modal.com"""
-    from huggingface_hub import snapshot_download
-    from transformers.utils import move_cache
-
-    # Account for strings
-    model_dir = Path(model_dir) if isinstance(model_dir, str) else model_dir
-
-    # Make directories
-    model_dir.mkdir(parents=True, exist_ok=True)
-
-    # Download via huggingface
-    snapshot_download(
-        model_name,
-        local_dir=model_dir,
-        ignore_patterns=["*.pt", "*.bin"],  # Using safetensors
-        token=os.environ["HF_TOKEN"],
+def init_modal_instance():
+    return App(
+        "whila-app",
+        image=_DEFAULT_IMAGE.pip_install(*_PIP_PACKAGES),
+        volumes=_DEFAULT_VOLUME,
+        gpu_config=_DEFAULT_GPU_CONFIG,
     )
 
-    # Move models
-    move_cache()
 
+class ModalApp:
 
-def _init_image(
-    model_dir, model_names, python_version: str = "3.10", timeout: int = 1200
-) -> Image:
+    def __init__(self):
+        self.textifier = Textifier()
+        self._create_gradio_interface()
 
-    # Create base image template with required modules
-    image = (
-        Image.debian_slim(python_version=python_version)
-        .pip_install(
-            "vllm==0.4.2",
-            "torch==2.3.0",
-            "transformers==4.40.2",
-            "ray==2.10.0",
-            "hf-transfer==0.1.6",
-            "huggingface_hub==0.22.2",
-        )
-        .env({"HF_HUB_ENABLE_HF_TRANSFER": "1"})
-    )
-
-    # Download models
-    for model_name in model_names:
-        image.run_function(
-            download_model_to_image,
-            timeout=timeout,
-            kwargs={"model_dir": model_dir, "model_name": model_name},
-            secrets=[modal.Secret.from_name("huggingface-secret")],
+    def _create_gradio_interface(self):
+        self.interface = gradio.Interface(
+            self.textifier.to_gradio(),
+            gradio.Audio(sources=["microphone"]),
+            "text",
         )
 
-
-def _init_app(image, tts_model) -> App: ...
-
-
-def init_modal(tts_model: str = "openai/whisper-medium.en", gpu=None):
-    app = App()
+    def launch(self, live=False):
+        self.interface.launch(live=live)
 
 
 # Footnotes:
